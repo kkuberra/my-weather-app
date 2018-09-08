87d13fdd07d25ae9bc3d2dd3910b4cbb48c9eede
Kyle Kuberra <kkuberra@gmail.com>
2018-09-07 18:43:04 -0600

    appling API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "fbd65baeac0d45157841efc6042bcac8";
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    
+  }
   render() {
     return (
       <div>
@@ -13,7 +20,7 @@ class App extends React.Component {
         <Form />
         <Weather /> 
       </div>
-    )
+    )  
   }
 }
 
