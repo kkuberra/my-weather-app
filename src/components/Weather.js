a6a55a33348e0daa86f4def3cfd5cfd305e24fc1
Kyle Kuberra <kkuberra@gmail.com>
2018-09-17 22:33:39 -0600

    adding API info

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component {
     render() { 
         return (
             <div>
-                { this.props.temperature }
+                {this.props.city}
+                {this.props.country}
+                {this.props.temperature}
+                {this.props.hunidity}
+                {this.props.description}
             </div>
         );
     }
