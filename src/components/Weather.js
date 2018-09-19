00bed06e8800e0f391dea43c6023b80bbce8bb7e
Kyle Kuberra <kkuberra@gmail.com>
2018-09-18 23:34:41 -0600

    added error functionality

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-class Weather extends React.Component {
-    render() { 
-        return (
-            <div>
-                {this.props.city}
-                {this.props.country}
-                {this.props.temperature}
-                {this.props.hunidity}
-                {this.props.description}
-            </div>
-        );
-    }
-}
- 
+const Weather = props => (
+    <div>
+        {props.city && props.country && <p>location:  {props.city}, {props.country}</p>}
+        {props.temperature && <p>temperature: {props.temperature}</p>}
+        {props.humidity && <p> Humidity: {props.hunidity}</p>}
+        {props.description && <p>Conditions: {props.description}</p>}
+        {props.error && <p>{props.error}</p>}
+    </div>
+)
+   
 export default Weather;
